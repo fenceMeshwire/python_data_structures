679e9a080676ddbbe1ccf1e7a37c4836e87aaecd
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-08-29 21:48:58 +0200

    Create 05_sorting_dict_lambda.py

---
+++ 05_sorting_dict_lambda.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# 05_sorting_dict_lambda.py
+
+stocks = [
+    {'stock': 'Unilever', 'value': 47, 'isin': 'GB00B10RZP78'},
+    {'stock': 'Golden Ocean', 'value': 10, 'isin': 'BMG396372051'},
+    {'stock': 'Altria', 'value': 46, 'isin': 'US02209S1033'},
+    {'stock': 'Imperial Brands', 'value': 22, 'isin': 'GB0004544929'},
+]
+
+stocks_by_stock = sorted(stocks, key=lambda s: s['stock'])
+print(stocks_by_stock)
+
+stocks_by_value = sorted(stocks, key=lambda v: v['value'])
+print(stocks_by_value)
