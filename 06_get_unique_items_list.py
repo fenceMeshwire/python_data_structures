3772f7eed295bdfc96fe7e0a929b06f4f82995a7
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-08-29 22:43:09 +0200

    Update 06_get_unique_items_list.py

--- 06_get_unique_items_list.py
+++ 06_get_unique_items_list.py
@@ -6,10 +6,13 @@
 
 duplicates = ['Paul', 'Jenny', 'Sally', 'James', 'Paul', 'Sam', 'Sam', 'Sally', 'Jenny']
 
+# First convert the list to a set, to get the unique items:
 unicates_set = set(duplicates)
 print(unicates_set)
 
+# Next convert the set back to a list:
 unicates_list = list(set(unicates_set))
 print(unicates_list)
 
+# Finally sort the list of unique items:
 print(sorted(unicates_list))
