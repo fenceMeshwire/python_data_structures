b48b9992dfe1128378fc636ab7d3a4e6466ec531
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2023-01-10 08:07:18 +0100

    Create 14_list_of_tuples_to_dict.py

---
+++ 14_list_of_tuples_to_dict.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# 14_list_of_tuples_to_dict.py
+
+# Purpose: Convert a list of tuples to a dictionary:
+data = [('a', 1), ('b', 2), ('c', 3), ('d', 4)]
+dct = dict(data)
+
+print(dct) # {'a': 1, 'b': 2, 'c': 3, 'd': 4}
